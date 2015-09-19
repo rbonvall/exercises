2691516babbec52852a6764ebbaf8ed57f6ba473
Roberto Bonvallet <rbonvall@gmail.com>
2015-09-19 12:12:20 -0400

    Implementation for multi{insert{R,L},subst}

--- programs.rkt
+++ programs.rkt
@@ -65,9 +65,31 @@
     [else              (cons (car lat)
                              (multirember a (cdr lat)))]))
 
-(define (multiinsertR new old lat) '())
-(define (multiinsertL new old lat) '())
-(define (multisubst new old lat) '())
+(define (multiinsertR new old lat)
+  (cond
+    [(null? lat) '()]
+    [(eq? old (car lat)) (cons old
+                               (cons new
+                                     (multiinsertR new old (cdr lat))))]
+    [else                (cons (car lat)
+                               (multiinsertR new old (cdr lat)))]))
+
+(define (multiinsertL new old lat)
+  (cond
+    [(null? lat) '()]
+    [(eq? old (car lat)) (cons new
+                               (cons old
+                                     (multiinsertL new old (cdr lat))))]
+    [else                (cons (car lat)
+                               (multiinsertL new old (cdr lat)))]))
+
+(define (multisubst new old lat)
+  (cond
+    [(null? lat) '()]
+    [(eq? old (car lat))  (cons new
+                                (multisubst new old (cdr lat)))]
+    [else                 (cons (car lat)
+                                (multisubst new old (cdr lat)))]))
 
 (define (plus a b)
   (cond

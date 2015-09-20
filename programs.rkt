c867451573514bae2c90d0243f3ec6d85c80010a
Roberto Bonvallet <rbonvall@gmail.com>
2015-09-19 23:54:01 -0400

    eqlist? from the book

--- programs.rkt
+++ programs.rkt
@@ -262,3 +262,16 @@
                     (list? (car l2))
                     (eqlist? (car l1) (car l2))))
            (eqlist? (cdr l1) (cdr l2)))))
+
+; eqlist? from the book
+(define (book-eqlist? l1 l2)
+  (cond
+    [(and (null? l1) (null? l2)) #t]
+    [(or  (null? l1) (null? l2)) #f]
+    [(and (atom? (car l1))
+          (atom? (car l2)))     (and (eqan?        (car l1) (car l2))
+                                     (book-eqlist? (cdr l1) (cdr l2)))]
+    [(or  (atom? (car l1))
+          (atom? (car l2)))     #f]
+    [else                       (and (book-eqlist? (car l1) (car l2))
+                                     (book-eqlist? (cdr l1) (cdr l2)))]))

bedfbebc1ea251312b3050a5e036b15bccbbb15f
Roberto Bonvallet <rbonvall@gmail.com>
2016-07-02 09:35:51 -0300

    Exercise 2.2: isSorted

--- fp-in-scala/src/main/scala/fp/ch2/Chapter2.scala
+++ fp-in-scala/src/main/scala/fp/ch2/Chapter2.scala
@@ -12,4 +12,15 @@ object Chapter2 {
     go(n, 0, 1)
   }
 
+  // @annotation.tailrec
+  // def isSorted[A](as: Array[A], ordered: (A, A) ⇒ Boolean): Boolean = {
+  //   if (as.length < 2)               true
+  //   else if (!ordered(as(0), as(1))) false
+  //   else                             isSorted(as.tail, ordered)
+  // }
+
+  @annotation.tailrec
+  def isSorted[A](as: Array[A], ordered: (A, A) ⇒ Boolean): Boolean =
+    as.length < 2 || ordered(as(0), as(1)) && isSorted(as.tail, ordered)
+
 }
--- fp-in-scala/src/test/scala/fp/ch2/Chapter2Spec.scala
+++ fp-in-scala/src/test/scala/fp/ch2/Chapter2Spec.scala
@@ -17,6 +17,18 @@ class Chapter2Spec extends FunSpec {
       assert(fib(7) === 13)
       assert(fib(8) === 21)
       assert(fib(9) === 34)
+    }
+  }
+
+  describe("isSorted") {
+    it("checks whether an Array[A] is sorted according to a given comparison function") {
+      assert( isSorted[Int](Array(1, 2, 3, 4, 5), _ <= _))
+      assert(!isSorted[Int](Array(1, 2, 3, 4, 5), _ >= _))
+      assert( isSorted[Int](Array(6, 2, 4, 5, 3, 1), _ % 2 <= _ % 2))
+      assert(!isSorted[Int](Array(1, 2, 3, 4, 5, 6), _ % 2 <= _ % 2))
+      assert( isSorted[String](Array("black", "blue", "red", "yellow"), _ <= _))
+      assert(!isSorted[String](Array("black", "blue", "red", "yellow"), _.length <= _.length))
+      assert( isSorted[String](Array("red", "blue", "black", "yellow"), _.length <= _.length))
 
     }
   }

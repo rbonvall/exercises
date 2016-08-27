723468abbba300f4880aab131bb3d2e1f55814bb
Roberto Bonvallet <rbonvall@gmail.com>
2016-08-27 20:42:59 -0300

    Exercises 3.5-3.11 with tests

--- fp-in-scala/src/main/scala/fp/ch3/Chapter3.scala
+++ fp-in-scala/src/main/scala/fp/ch3/Chapter3.scala
@@ -20,5 +20,45 @@ object Chapter3 {
     if (n == 0) xs
     else        drop(tail(xs), n - 1)
 
+  // Exercise 3.5
+  @annotation.tailrec
+  def dropWhile[T](xs: List[T], f: T ⇒ Boolean): List[T] = xs match {
+    case first :: rest if f(first) ⇒ dropWhile(rest, f)
+    case _                         ⇒ xs
+  }
+
+  // Exercise 3.6
+  def init[T](xs: List[T]): List[T] = xs match {
+    case first :: last :: Nil ⇒ first :: Nil
+    case first :: rest        ⇒ first :: init(rest)
+    case Nil ⇒ ???
+  }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) ⇒ B): B = as match {
+    case Nil ⇒ z
+    case x :: xs ⇒ f(x, foldRight(xs, z)(f))
+  }
+  def product(ns: List[Double]) = foldRight(ns, 1.0) { _ * _ }
+
+  // Exercise 3.7: I don't think is possible we can get short-circuiting
+  // using foldRight to define product.
+
+  // Exercise 3.8
+  def fons[A](as: List[A]) = foldRight(as, List[A]())(_ :: _)
+
+  // Exercise 3.9
+  def length[A](as: List[A]): Int = foldRight(as, 0) { (_, a) ⇒ a + 1 }
+
+  // Exercise 3.10
+  @annotation.tailrec
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) ⇒ B): B = as match {
+    case Nil ⇒ z
+    case first :: rest ⇒ foldLeft(rest, f(z, first))(f)
+  }
+
+  // Exercise 3.11
+  def sumL(ns: List[Int]): Int = foldLeft(ns, 0) { _ + _ }
+  def productL(xs: List[Double]): Double = foldLeft(xs, 1.0) { _ * _ }
+  def lengthL[A](as: List[A]): Int = foldLeft(as, 0) { (a, _) ⇒ a + 1 }
 
 }
--- fp-in-scala/src/test/scala/fp/ch3/Chapter3Spec.scala
+++ fp-in-scala/src/test/scala/fp/ch3/Chapter3Spec.scala
@@ -1,6 +1,7 @@
 package fp.ch3
 
 import org.scalatest.FunSpec
+import org.scalactic.TolerantNumerics
 
 class Chapter3Spec extends FunSpec {
   import Chapter3._
@@ -20,13 +21,72 @@ class Chapter3Spec extends FunSpec {
   }
 
   describe("drop") {
-    it("drops") {
+    it("drops as many elements as asked") {
       assert(drop(xs, 0) === xs)
       assert(drop(xs, 1) === List("b", "c", "d"))
       assert(drop(xs, 2) === List("c", "d"))
       assert(drop(xs, 3) === List("d"))
     }
   }
+
+  describe("dropWhile") {
+    it("drops elements while the predicate is true") {
+      assert(dropWhile(xs, { x: String ⇒ x != "c" }) === List("c", "d"))
+      assert(dropWhile(xs, { x: String ⇒ false }) === xs)
+      assert(dropWhile(xs, { x: String ⇒ true  }) === Nil)
+      assert(dropWhile(List[Int](), { x: Int ⇒ x > 2 }) === Nil)
+    }
+  }
+
+  describe("init") {
+    it("gives all elements but the last one") {
+      assert(init(xs) === List("a", "b", "c"))
+    }
+  }
+
+  describe("folding with cons") {
+    it("keeps the list unchanged") {
+      val ns = List(1, 2, 3)
+      assert(fons(ns) === ns)
+
+    }
+  }
+
+  describe("length") {
+    it("computes the length of a list") {
+      assert(length(Nil) === 0)
+      assert(length(xs) === 4)
+    }
+  }
+
+  describe("foldLeft") {
+    it("folds") {
+      assert(foldLeft(xs, "")(_ ++ _) === "abcd")
+    }
+  }
+
+  describe("sumL") {
+    it("adds integers by folding from the left") {
+      assert(sumL(Nil) === 0)
+      assert(sumL(List(66, 11, 22)) === 99)
+    }
+  }
+
+  describe("productL") {
+    it("multiplies elements by folding from the left") {
+      implicit val doubleEq = TolerantNumerics.tolerantDoubleEquality(1e-2)
+      assert(productL(Nil) === 1.0)
+      assert(productL(List(10.0, 1.0, 3.14)) === 31.4)
+    }
+  }
+
+  describe("lengthL") {
+    it("counts stuff by folding from the left") {
+      assert(lengthL(Nil) === 0)
+      assert(lengthL(xs) === 4)
+    }
+  }
+
 
 
 }

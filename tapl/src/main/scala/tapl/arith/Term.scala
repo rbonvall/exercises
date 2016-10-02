b7d9aaefe4d5d392f40db2cc37a91ac611eaab0c
Roberto Bonvallet <rbonvall@gmail.com>
2016-10-02 14:25:23 -0300

    Single-step evaluator for arith

--- tapl/src/main/scala/tapl/arith/Term.scala
+++ tapl/src/main/scala/tapl/arith/Term.scala
@@ -46,9 +46,46 @@ object Term {
     case Succ(t)       ⇒ depth(t) + 1
     case Pred(t)       ⇒ depth(t) + 1
     case IsZero(t)     ⇒ depth(t) + 1
-    case Cond(t, u, v) ⇒ List(depth(t), depth(u), depth(v)).ma + 1
+    case Cond(t, u, v) ⇒ List(depth(t), depth(u), depth(v)).max + 1
   }
 
+  def isNumerical(t: Term): Boolean = t match {
+    case Zero    ⇒ true
+    case Succ(t) ⇒ isNumerical(t)
+    case _       ⇒ false
+  }
+
+  def isVal(t: Term): Boolean = t match {
+    case True | False ⇒ true
+    case t            ⇒ isNumerical(t)
+  }
+
+  case object NoRuleApplies extends Exception
+
+  def eval1(t: Term): Term = t match {
+    case Cond(True,  t2, t3) ⇒ t2
+    case Cond(False, t2, t3) ⇒ t3
+    case Cond(t1,    t2, t3) ⇒ Cond(eval1(t1), t2, t3)
+
+    case Succ(t1) ⇒ Succ(eval1(t1))
+
+    case Pred(Zero)                          ⇒ Zero
+    case Pred(Succ(nv1)) if isNumerical(nv1) ⇒ nv1
+    case Pred(t1)                            ⇒ Pred(eval1(t1))
+
+    case IsZero(Zero)                          ⇒ True
+    case IsZero(Succ(nv1)) if isNumerical(nv1) ⇒ False
+    case IsZero(t1)                            ⇒ IsZero(eval1(t1))
+
+    case _ ⇒ throw NoRuleApplies
+  }
+
+  def eval(t: Term): Term = try {
+    val result = eval1(t)
+    eval(result)
+  } catch {
+    case NoRuleApplies ⇒ t
+  }
 
 
 }

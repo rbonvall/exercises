34ff14102d11a43e3a69b6117ff2ef84f32b3a33
Roberto Bonvallet <rbonvall@gmail.com>
2016-10-18 21:46:40 -0300

    Data types and function stubs for the untyped λ-calculus

---
+++ tapl/src/main/scala/tapl/untyped/Term.scala
@@ -0,0 +1,32 @@
+package tapl.untyped
+
+sealed trait Info
+case object DummyInfo extends Info
+
+sealed trait Binding
+case object NameBind extends Binding
+
+sealed trait Term {
+  def repr(ctx: Term.Context): String = this match {
+    case Term.Abs(body, name) ⇒
+      val (ctx2, x) = pickFreshName(ctx, name)
+      s"(λ$x . ${body.repr(ctx2)})"
+    case Term.App(fn, arg) ⇒
+      s"(${fn.repr(ctx)} ${arg.repr(ctx)})"
+    case Term.Var(n) ⇒
+      indexToName(ctx, n)
+  }
+
+  def pickFreshName(ctx: Term.Context, name: String): (Term.Context, String) = ???
+  def indexToName(ctx: Term.Context, n: Int): String = ???
+}
+object Term {
+  // n is the de Bruijn index
+  case class Var(n: Int)                   extends Term
+  case class Abs(body: Term, name: String) extends Term
+  case class App(fn: Term, arg: Term)      extends Term
+
+  type Context = List[(String, Binding)]
+}
+
+

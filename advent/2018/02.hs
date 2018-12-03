ee0f34ecebe0dc4a2e66a887353014b101934540
Roberto Bonvallet <roberto@mez.cl>
2018-12-03 15:13:26 -0300

    AoC'18, 2.2

--- advent/2018/02.hs
+++ advent/2018/02.hs
@@ -1,4 +1,5 @@
 import qualified Data.Map as Map
+import Data.List (sort)
 import System.IO
 
 count xs = foldl updateCount Map.empty xs
@@ -7,13 +8,27 @@ count xs = foldl updateCount Map.empty xs
 hasTwoAndThree = foldl f (False, False)
   where f (has2, has3) n = (has2 || n == 2, has3 || n == 3)
 
-part1 ids = fst c * snd c
+commonLetters v w = map fst $ filter areEqual $ zip v w
+  where areEqual (x, y) = x == y
+
+part1 ids =
+  let c = foldl f (0, 0) $ map (hasTwoAndThree . count) ids
+  in  fst c * snd c
   where
     toInt b = if b then 1 else 0
     f (c2, c3) (has2, has3) = (c2 + toInt has2, c3 + toInt has3)
-    c = foldl f (0, 0) $ map (hasTwoAndThree . count) ids
 
-part2 ids = ""
+part2 ids =
+  let sortedPairs        = pairs $ sort $ ids
+      sortedShiftedPairs = pairs $ sort $ map shift ids
+      allSortedPairs     = sortedPairs ++ sortedShiftedPairs
+      commonParts        = map (uncurry commonLetters) allSortedPairs
+  in  filter isLongest commonParts
+  where
+    shift (x : xs) = xs ++ [x]
+    pairs l@(_ : xs) = zip l xs
+    n = length $ head ids
+    isLongest w = length w == n - 1
 
 main = do
   handle <- openFile "02.txt" ReadMode

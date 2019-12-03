0fe1b30c30a90e9d27a3cae69d9f6562ac7df347
Roberto Bonvallet <roberto@mez.cl>
2019-12-03 00:35:52 -0300

    AoC'19 01.2

--- advent/2019/01.hs
+++ advent/2019/01.hs
@@ -6,12 +6,12 @@ fuelNeeded m = (m `div` 3) - 2
 
 part1 = sum . map fuelNeeded
 
-part2 = \x -> 0
+part2 = sum . concatMap (tail . takeWhile (>0) . iterate fuelNeeded)
 
 main = do
   handle <- openFile "01.txt" ReadMode
   contents <- hGetContents handle
-  let moduleMasses = map (read :: String -> Int) (lines contents)
+  let moduleMasses = map (read :: String -> Integer) (lines contents)
   print $ map fuelNeeded examples
   print $ part1 moduleMasses
   print $ part2 moduleMasses

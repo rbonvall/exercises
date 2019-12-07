82abedb8d8c44d01e7be1fd0efb2d405c2a09761
Roberto Bonvallet <roberto@mez.cl>
2019-12-07 17:34:16 -0300

    AoC'19: improve 3.1, solve 3.2

--- advent/2019/03.hs
+++ advent/2019/03.hs
@@ -1,5 +1,6 @@
 import Data.Char (isDigit)
 import qualified Data.Set as Set
+import qualified Data.Map.Strict as Map
 import System.IO
 
 data Direction = U | D | L | R deriving Show
@@ -17,16 +18,6 @@ instance Read Move where
       charToDir 'R' = R
   readsPrec _ [] = []
 
-example1 = (parseWire "R8,U5,L5,D3",
-            parseWire "U7,R6,D4,L4",
-            6)
-example2 = (parseWire "R75,D30,R83,U83,L12,D49,R71,U7,L72",
-            parseWire "U62,R66,U55,R34,D71,R55,D58,R83",
-            159)
-example3 = (parseWire "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
-            parseWire "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
-            135)
-
 withX x y = (x, y)
 withY y x = (x, y)
 
@@ -41,22 +32,64 @@ path moves = path' moves [(0, 0)]
   where
     path' []     acc = acc
     path' (m:ms) acc = path' ms (acc ++ segment (last acc) m)
-
-intersections w1 w2 =
-  let s1 = Set.fromList (path w1)
-      s2 = Set.fromList (path w2)
-  in Set.toList $ Set.intersection s1 s2
+    -- inefficient: gets last element of linked list
 
 parseWire :: String -> [Move]
 parseWire line = read ("[" ++ line ++ "]")
 
+manhattan :: (Int, Int) -> (Int, Int) -> Int
 manhattan (x0, y0) (x1, y1) = abs (x1 - x0) + abs (y1 - y0)
 
-part1 w1 w2 = minimum $ filter (>0) $ map (manhattan (0, 0)) $ intersections w1 w2
+stepMap :: [Move] -> Map.Map (Int, Int) Int
+stepMap w =
+  let p = tail $ path w
+  in  Map.fromListWith min $ zip p [1..]
+
+part1 :: [Move] -> [Move] -> Int
+part1 w1 w2 =
+  let s1 = Set.fromList (tail $ path w1)
+      s2 = Set.fromList (tail $ path w2)
+      crossings = Set.intersection s1 s2
+  in minimum $ Set.map (manhattan (0, 0)) crossings
+
+part2 :: [Move] -> [Move] -> Int
+part2 w1 w2 =
+  let sm1 = stepMap w1
+      sm2 = stepMap w2
+  in  minimum $ Map.intersectionWith (+) sm1 sm2
+
+example1 = (parseWire "R8,U5,L5,D3",
+            parseWire "U7,R6,D4,L4")
+example2 = (parseWire "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+            parseWire "U62,R66,U55,R34,D71,R55,D58,R83")
+example3 = (parseWire "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+            parseWire "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
+example4 = (parseWire "R3,U7,L1,D2,L3,D6,R3,U3,R6",
+            parseWire "U5,R4,D1,L8,D6,R7,D1,R2,U6")
+
+printExample e = do
+  print $ uncurry part1 $ e
+  print $ uncurry part2 $ e
 
 main = do
+  putStrLn "Example 1"
+  printExample example1
+
+  putStrLn "Example 2"
+  printExample example2
+
+  putStrLn "Example 3"
+  printExample example3
+
+  putStrLn "Example 4"
+  printExample example4
+
   handle <- openFile "03.txt" ReadMode
   contents <- hGetContents handle
   let [w1, w2] = map parseWire $ lines contents
+
+  putStrLn "Problem input"
   print $ part1 w1 w2
+  print $ part2 w1 w2
+
   hClose handle

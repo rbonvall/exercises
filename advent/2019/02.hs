170a73eba76a259554fab1af9bd3d70b90d1415d
Roberto Bonvallet <roberto@mez.cl>
2019-12-19 22:42:29 -0300

    AoC'19, 2.1. First attempt.

--- advent/2019/02.hs
+++ advent/2019/02.hs
@@ -1,13 +1,35 @@
 import System.IO
-import Data.Sequence
+import Data.Sequence (Seq, index, update, fromList)
+import Data.Maybe (isJust)
 
-data State = State Int (Seq Int)
+data State = State Int (Seq Int) deriving Show
 
-part1 initialState = 0
+example :: Seq Int
+example = fromList [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]
+
+next :: State -> Maybe State
+next (State pos ops) =
+  let w = ops `index` pos
+      x = ops `index` (ops `index` (pos + 1))
+      y = ops `index` (ops `index` (pos + 2))
+      z = ops `index` (ops `index` (pos + 3))
+  in execute ops w x y z
+  where 
+    execute ops  1 x y z = Just (State (pos + 4) (update z (x + y) ops))
+    execute ops  2 x y z = Just (State (pos + 4) (update z (x * y) ops))
+    execute ops 99 _ _ _ = Nothing
+
+part1 ops =
+  let restoredOps = (update 1 12 . update 2 2) ops
+      s0 = State 0 restoredOps
+      ss = takeWhile isJust $ iterate (next =<<) (Just s0)
+      sf = last $ takeWhile isJust ss
+  in valueInPositionZero sf
+  where valueInPositionZero (Just (State _ ops)) = index ops 0
 
 main = do
   handle <- openFile "02.txt" ReadMode
   contents <- hGetContents handle
   let opList = read ("[" ++ contents ++ "]") :: [Int]
-  print $ part1 $ State 0 (fromList opList)
+  print $ part1 (fromList opList)
   hClose handle

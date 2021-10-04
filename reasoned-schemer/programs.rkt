0a9cef9ed667a6450a91b956fadc5808eb038b86
Roberto Bonvallet <roberto@mez.cl>
2021-10-04 02:00:54 +0000

    Rename to uppercase to avoid clash with rackunit 'fail'

--- reasoned-schemer/programs.rkt
+++ reasoned-schemer/programs.rkt
@@ -4,19 +4,19 @@
 
 ; From https://www.monolune.com/using-racket-for-the-reasoned-schemer/
 (require minikanren)
-(define succeed (== #t #t))
-(define fail    (== #t #f))
-(define else succeed)
+(define SUCCEED (== #t #t))
+(define FAIL    (== #t #f))
+(define else SUCCEED)
 (current-readtable
   (make-readtable (current-readtable)
                   #\s
                   'dispatch-macro
-                  (lambda (a b c d e f) succeed)))
+                  (lambda (a b c d e f) SUCCEED)))
 (current-readtable
   (make-readtable (current-readtable)
                   #\u
                   'dispatch-macro
-                  (lambda (a b c d e f) fail)))
+                  (lambda (a b c d e f) FAIL)))
 
 
 

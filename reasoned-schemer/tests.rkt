ab349022d40e99575574d457ccf063857ff08c8c
Roberto Bonvallet <roberto@mez.cl>
2021-10-19 15:32:34 +0000

    Chapter 1 examples up to frame 49

--- reasoned-schemer/tests.rkt
+++ reasoned-schemer/tests.rkt
@@ -31,4 +31,59 @@
   ; Commas in a backtick expression can only precede variables.
   ; What is not a variable behaves as if it were quoted.
 
+  (check-equal? (run* (q)
+                  (fresh (x)
+                    (== x q)))           '(_.0))
+  ; x and q are fused, but remain fresh.
+  ; They get the same association.
+
+  (check-equal? (run* (q)
+                  (== '(((pea)) pod)
+                      '(((pea)) pod)))   '(_.0))
+
+  (check-equal? (run* (q)
+                  (== '(((pea)) pod)
+                      `(((pea))  ,q)))   '(pod))
+
+  (check-equal? (run* (q)
+                  (== `((( ,q)) pod)
+                      '(((pea)) pod)))   '(pea))
+
+  (check-equal? (run* (q)
+                  (fresh (x)
+                    (== `(((,q)) pod)
+                        `(((,x)) pod)))) '(_.0))
+  ; q remains fresh, even though x is fused with it.
+
+  (check-equal? (run* (q)
+                  (fresh (x)
+                    (== `(((,q))  ,x)
+                        `(((,x)) pod)))) '(pod))
+  ; pod is associated with x, and x is fused with q.
+
+  (check-equal? (run* (q)
+                  (fresh (x)
+                    (== `(,x ,x) q)))    '((_.0 _.0)))
+  ; Every instance of the same fresh variable
+  ; is replaced by the same reified variable.
+
+  (check-equal? (run* (q)
+                  (fresh (x)
+                    (fresh (y)
+                      (== `(,q      ,y)
+                          `((,x ,y) ,x))))) '((_.0 _.0)))
+  ; The value of `(,x ,y) is associated with q.
+  ; y is fused with x, making them the same.
+
+  (check-equal? (run* (q)
+                  (fresh (x)
+                    (fresh (y)
+                      (== `(,x ,y) q)))) '((_.0 _.1)))
+  ; Each different variable is reified with a different subscript.
+
+  (check-equal? (run* (q)
+                  (fresh (x)
+                    (fresh (y)
+                      (== `(,x ,y ,x) q)))) '((_.0 _.1 _.0)))
+
 )

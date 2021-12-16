e7564c548f2d408aa0ca5dad28ec81b4826a9eb6
Roberto Bonvallet <roberto@mez.cl>
2021-12-16 18:05:33 -0300

    AoC'21: progress for 9.2, basin calculation yet to be implemented

--- advent/2021/09.scala
+++ advent/2021/09.scala
@@ -1,28 +1,40 @@
-import scala.collection.immutable.ArraySeq
+def findLowPoints(heightMap: Map[(Int, Int), Int]): Set[(Int, Int)] =
+  val heightMapWithWall = heightMap.withDefaultValue(Int.MaxValue)
+  heightMapWithWall
+    .filter { case ((i, j), height) =>
+      heightMapWithWall(i + 1, j    ) > height &&
+      heightMapWithWall(i - 1, j    ) > height &&
+      heightMapWithWall(i    , j + 1) > height &&
+      heightMapWithWall(i    , j - 1) > height
+    }
+    .keySet
+
+def findBasin(heightMap: Map[(Int, Int), Int], lowPoint: (Int, Int)): Set[(Int, Int)] =
+  // TODO: implement.
+  Set(lowPoint)
 
 def part1(input: Map[(Int, Int), Int]) =
-  val inputWithWall = input.withDefaultValue(Int.MaxValue)
-  inputWithWall
-    .filter { case ((i, j), height) =>
-      inputWithWall(i + 1, j    ) > height &&
-      inputWithWall(i - 1, j    ) > height &&
-      inputWithWall(i    , j + 1) > height &&
-      inputWithWall(i    , j - 1) > height
-    }
-    .map { case ((i, j), height) => height + 1 }
+  findLowPoints(input)
+    .to(Seq)
+    .map((i, j) => input(i, j) + 1)
     .sum
 
-def part2(x: Any) =
-  ()
+def part2(input: Map[(Int, Int), Int]) =
+  findLowPoints(input)
+    .to(Seq)
+    .map(p => findBasin(input, p).size)
+    .sortBy(n => -n)
+    .take(3)
+    .product
 
 @main
 def run =
   val src = io.Source.fromFile("09.txt")
-  val matrix: ArraySeq[ArraySeq[Int]] =
+  val matrix: List[List[Int]] =
     try src.getLines
       .filter(_.nonEmpty)
-      .map(_.map(_.toString.toInt).to(ArraySeq))
-      .to(ArraySeq)
+      .map(_.map(_.toString.toInt).to(List))
+      .to(List)
     finally src.close()
 
   val asPairs =

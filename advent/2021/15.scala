c47bf6183da15beef4213776340980c95275f049
Roberto Bonvallet <roberto@mez.cl>
2022-01-09 13:26:50 -0300

    Skeletons for days 11, 13-16

---
+++ advent/2021/15.scala
@@ -0,0 +1,19 @@
+type Input = Any
+
+def part1(input: Input) =
+  ()
+
+def part2(input: Input) =
+  ()
+
+@main
+def run =
+  val src = io.Source.fromFile("15.txt")
+  val input: Input =
+    try src.getLines.toList
+    finally src.close()
+
+  println(part1(input))
+  println(part2(input))
+
+

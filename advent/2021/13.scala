889fb9d36061c9abf46cb2b2f9fc28de15b42ccc
Roberto Bonvallet <roberto@mez.cl>
2022-01-09 14:13:29 -0300

    AoC'21, day 13: parse input

--- advent/2021/13.scala
+++ advent/2021/13.scala
@@ -1,4 +1,23 @@
-type Input = Any
+enum Axis:
+  case X, Y
+
+case class Input(
+  dots: List[(Int, Int)],
+  folds: List[(Axis, Int)],
+)
+object Input:
+  val dotLine = """(\d+),(\d+)""".r
+  val foldInstruction = """fold along (x|y)=(\d+)""".r
+  def fromLines(lines: List[String]): Input =
+    Input(
+      dots = lines.collect {
+        case dotLine(x, y) => (x.toInt, y.toInt)
+      },
+      folds = lines.collect {
+        case foldInstruction("x", x) => (Axis.X, x.toInt)
+        case foldInstruction("y", y) => (Axis.Y, y.toInt)
+      },
+    )
 
 def part1(input: Input) =
   ()
@@ -10,10 +29,14 @@ def part2(input: Input) =
 def run =
   val src = io.Source.fromFile("13.txt")
   val input: Input =
-    try src.getLines.toList
+    try Input.fromLines(src.getLines.toList)
     finally src.close()
+
+  assert(input.dots.head == (994, 18))
+  assert(input.dots.last == (339, 28))
+  assert(input.folds.head == (Axis.X, 655))
+  assert(input.folds.last == (Axis.Y, 6))
 
   println(part1(input))
   println(part2(input))
-
 

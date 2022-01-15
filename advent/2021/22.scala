c20939241b113f04e62886656d48391a64502bbf
Roberto Bonvallet <roberto@mez.cl>
2022-01-15 20:56:42 -0300

    AoC'21, failed attempt at 22.2

--- advent/2021/22.scala
+++ advent/2021/22.scala
@@ -62,14 +62,27 @@ def runSteps(steps: List[RebootStep]): Set[(Int, Int, Int)] =
       case Status.Off => onCubes diff  cubesToSwitch
   }
 
+def ticksFromRange(r: Range): List[Int] =
+  List(r.head, r.last + 1)
+
+def emptyRegionMapFromThreeDeeRanges(threeDeeRanges: List[ThreeDeeRange]): CubeRegionMap =
+  val xTicks = threeDeeRanges.flatMap(r => ticksFromRange(r.xRange)).to(Vector).distinct.sorted
+  val yTicks = threeDeeRanges.flatMap(r => ticksFromRange(r.yRange)).to(Vector).distinct.sorted
+  val zTicks = threeDeeRanges.flatMap(r => ticksFromRange(r.zRange)).to(Vector).distinct.sorted
+  CubeRegionMap(xTicks, yTicks, zTicks)
+
 def part1(input: Input) =
   runSteps(
     input.map(step => step.copy(cubes = step.cubes.clipTo(50)))
   ).size
 
 def part2(input: Input) =
-  // runSteps(input).size  // <-- OutOfMemoryError
-  ()
+  val initial = emptyRegionMapFromThreeDeeRanges(input.map(_.cubes))
+  println(s"${initial.xAxisTicks.length} x ${initial.yAxisTicks.length} x ${initial.zAxisTicks.length}")
+  input.foldLeft(initial) { case (regionMap, r@RebootStep(status, ThreeDeeRange(xs, ys, zs))) =>
+    println(r)
+    regionMap.setRegion(status == Status.On, xs, ys, zs)
+  }.totalSize
 
 @main
 def run =
@@ -82,4 +95,60 @@ def run =
   println(part2(input))
 
 
+// --- Implementation of a custom data structure for this problem ---
+
+import scala.collection.immutable.BitSet
+
+case class CubeRegionMap(
+  xAxisTicks: Vector[Int],
+  yAxisTicks: Vector[Int],
+  zAxisTicks: Vector[Int],
+  cubes: BitSet = BitSet.empty,
+):
+
+  val xAxisTickIndex: Map[Int, Int] = xAxisTicks.zipWithIndex.toMap
+  val yAxisTickIndex: Map[Int, Int] = yAxisTicks.zipWithIndex.toMap
+  val zAxisTickIndex: Map[Int, Int] = zAxisTicks.zipWithIndex.toMap
+
+  val X = xAxisTicks.size
+  val Y = yAxisTicks.size
+  val Z = zAxisTicks.size
+
+  def rank(i: Int, j: Int, k: Int): Int =
+    i + j * X + k * X * Y
+
+  def unrank(n: Int): (Int, Int, Int) =
+    val i = n % X
+    val j = (n / X) % Y
+    val k = (n / (X * Y)) % Z
+    (i, j, k)
+
+  def setRegion(on: Boolean, xRange: Range, yRange: Range, zRange: Range): CubeRegionMap =
+    val i0 = xAxisTickIndex(xRange.head)
+    val i1 = xAxisTickIndex(xRange.last + 1)
+    val j0 = yAxisTickIndex(yRange.head)
+    val j1 = yAxisTickIndex(yRange.last + 1)
+    val k0 = zAxisTickIndex(zRange.head)
+    val k1 = zAxisTickIndex(zRange.last + 1)
+    val regionRanks =
+      for
+        i <- i0 until i1
+        j <- j0 until j1
+        k <- k0 until k1
+      yield rank(i, j, k)
+    this.copy(
+      cubes =
+        if on
+        then cubes union regionRanks.toSet
+        else cubes diff  regionRanks.toSet
+    )
+
+  def cubeSize(i: Int, j: Int, k: Int): Int =
+    val dx = xAxisTicks(i + 1) - xAxisTicks(i)
+    val dy = yAxisTicks(j + 1) - yAxisTicks(j)
+    val dz = zAxisTicks(k + 1) - zAxisTicks(k)
+    dx * dy * dz
+
+  def totalSize =
+    cubes.iterator.map(unrank).map(cubeSize).sum
 

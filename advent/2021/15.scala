759ce8f29df328a6546f81c35c5b1e5c0622f913
Roberto Bonvallet <roberto@mez.cl>
2022-01-16 00:38:53 -0300

    AoC'21, day 15: read input

--- advent/2021/15.scala
+++ advent/2021/15.scala
@@ -1,4 +1,17 @@
-type Input = Any
+import scala.collection.immutable.ArraySeq
+
+case class Input(matrix: ArraySeq[ArraySeq[Int]]):
+  require(matrix.forall(_.length == matrix.head.length))
+  val height = matrix.length
+  val width = matrix.head.length
+
+  def get(i: Int, j: Int) =
+    matrix(i)(j)
+
+object Input:
+  def fromLines(lines: List[String]): Input =
+    Input(lines.to(ArraySeq).map(_.map(_.toString.toInt).to(ArraySeq)))
+
 
 def part1(input: Input) =
   ()
@@ -6,12 +19,18 @@ def part1(input: Input) =
 def part2(input: Input) =
   ()
 
+
 @main
 def run =
   val src = io.Source.fromFile("15.txt")
   val input: Input =
-    try src.getLines.toList
+    try Input.fromLines(src.getLines.filter(_.nonEmpty).toList)
     finally src.close()
+
+  assert(input.width  == 100)
+  assert(input.height == 100)
+  assert(input.get( 0,  0) == 8)
+  assert(input.get(99, 99) == 9)
 
   println(part1(input))
   println(part2(input))

20682cb6d83a2b3c593cae9725b72fe9b8514b58
Roberto Bonvallet <roberto@mez.cl>
2026-01-01 01:23:09 -0300

    Rewrite day 1 as a test suite

--- advent/2025/01.scala
+++ advent/2025/01.scala
@@ -1,4 +1,8 @@
 //> using scala 3.7.4
+//> using dep com.lihaoyi::pprint:0.9.6
+//> using dep org.scalameta::munit:1.2.1
+
+// $ scala-cli test 01.scala
 
 extension (n: Int)
     def mod(d: Int): Int = ((n % d) + d) % d
@@ -11,26 +15,11 @@ def parseRotation(line: String): Int =
 case class Dial(size: Int, pos: Int):
     def turn(rotation: Int) = copy(pos = (pos + rotation).mod(size))
 
-@main
-def run =
-    val src = io.Source.fromFile("01.txt")
-    val rotations =
-        try src.getLines.filterNot(_.isEmpty).map(parseRotation).to(Vector)
-        finally src.close()
+def part1(rotationsWithDial: Seq[(rotation: Int, dial: Dial)]) =
+    rotationsWithDial.count(_.dial.pos == 0)
 
-    // zip rotations with their resulting dial states
-    val rotationsWithDial: Seq[(rotation: Int, dial: Dial)] =
-        val s0 = (rotation = 0, dial = Dial(size = 100, pos = 50))
-        rotations.scanLeft(s0): (state, rot) =>
-            (rot, state.dial.turn(rot))
-
-    // part 1
-    println:
-        rotationsWithDial.count(_.dial.pos == 0)
-
-    // part 2
-    println:
-        rotationsWithDial.map(countPassesThroughZero).sum
+def part2(rotationsWithDial: Seq[(rotation: Int, dial: Dial)]) =
+    rotationsWithDial.map(countPassesThroughZero).sum
 
 def countPassesThroughZero(s: (rotation: Int, dial: Dial)): Int =
     val d =
@@ -44,4 +33,20 @@ def z(r: Int, d: Int, n: Int): Int =
     require(0 <= r)
     require(0 < n)
     (r + n - d - 1) / n
+
+class Test01 extends munit.FunSuite:
+    test("run"):
+        val src = io.Source.fromFile("01.txt")
+        val rotations =
+            try src.getLines.filterNot(_.isEmpty).map(parseRotation).to(Vector)
+            finally src.close()
+
+        // zip rotations with their resulting dial states
+        val rotationsWithDial: Seq[(rotation: Int, dial: Dial)] =
+            val s0 = (rotation = 0, dial = Dial(size = 100, pos = 50))
+            rotations.scanLeft(s0): (state, rot) =>
+                (rot, state.dial.turn(rot))
+
+        pprint.log(part1(rotationsWithDial))
+        pprint.log(part2(rotationsWithDial))
 
